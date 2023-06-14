3fd12b4c22e333e93b74d94c74b73a2babaa8172
Bruno-GUI <brunoguilhermefs81003@gmail.com>
2023-06-14 14:41:13 -0300

    commit "update"

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Obra } from "./types";
+
+export const listaLeitura: Obra[] = [
+  {
+    titulo: "Duke Pendragon",
+  },
+  {
+    titulo: "The World After End",
+  },
+  {
+    titulo: "Return of SSS Class Ranker",
+  },
+  {
+    titulo: "Leveling Up My Isekai'd Abs",
+  },
+  {
+    titulo: "Return of the Disaster Class",
+  },
+  {
+    titulo: "The World After End",
+  },
+  {
+    titulo: "The Return of the Frozen Player",
+  },
+  {
+    titulo: "Murim Login",
+  },
+  {
+    titulo: "Reincarnation of the Battle God",
+  },
+  {
+    titulo: "Standart of Reincarnation",
+  },
+  {
+    titulo: "The Dark Magician Transmigrates After 66666 Years",
+  },
+];
+
+export default listaLeitura;
